79a93d4cdc95c3789d8986642294b26c31c5c7db
jsanrub275 <a20sanrubj0433@ies-mardecadiz.com>
2021-01-05 20:02:20 +0100

    Modificacion poner todo modularizado (en funciones)

--- matriz_bidimensional.py
+++ matriz_bidimensional.py
@@ -1,49 +1,79 @@
-#Crear una matríz bidimensional de 7 x 8 y después de llenarla de números enteros, ordenarla de menor a mayor
+"""Crear una matríz bidimensional de 7 x 8 y después de llenarla de números enteros, ordenarla de menor a mayor"""
 
-#Módulo que implementar un generador de números pseudoaleatorio
-import random
+#Función principal
+def main():
+    #Módulo para poder generar números pseudoaleatorio
+    import random
 
-#Definir en variables el número de filas y columnas
-numero_minimo_random = 1
-numero_maximo_random = 9999
-filas = 7
-columnas = 8
+    #Definir el número de filas y columnas de la matriz bidimensional
+    filas = 7
+    columnas = 8
 
-#Generar una matriz de "i" filas por "j" columnas con un número random para cada elemento
-matriz = [[random.randint(numero_minimo_random, numero_maximo_random) for j in range(columnas)] for i in range(filas)]
+    #Definir el limite inferior y superior de valores para la generación de números pseudoaleatorios
+    numero_minimo_random = 1
+    numero_maximo_random = 9999
 
-#Se muestra el contenido de la mátriz antes de su ordenación
-print("Antes de ordenar: ")
-print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matriz]))
+    #Generar una matriz bidimensional, en el que el número de filas y columnas se definan a través de variables 
+    #Se genera un número pseudoaletorio para cada elmento que se crea
+    matriz = [[random.randint(numero_minimo_random, numero_maximo_random) for j in range(columnas)] for i in range(filas)]
 
-# Metodo de ordenación burbuja (se comparan todos con el siguiente y sólo se para cuando no se hace ningún intercambio)
-hay_cambios = True
-while(hay_cambios):
-    hay_cambios = False
-    
-    for i in range(filas):
-        for j in range(columnas):
-            #Se ignora el intentar comparar el último elemento con un inexistente siguiente
-            if(not(i == filas - 1 and j == columnas - 1)):
-                #Definimos índices para el segundo elemento a comparar
-                k = i
-                l = j + 1
+    #Se muestra el contenido de la mátriz antes de su ordenación
+    print("\nAntes de ordenar:\n")
+    dibujar_matriz(matriz)
 
-                #Si sobrepasa la última columna, cogemos el primer elemento de la siguiente fila 
-                if(l == columnas):
-                    k += 1
-                    l = 0
-
-                #Comparamos e intercambiamos si es mayor (y activamos flag)
-                if(matriz[i][j] >  matriz[k][l]):
-                    auxiliar = matriz[i][j]
-                    matriz[i][j] = matriz[k][l]
-                    matriz[k][l] = auxiliar
-
-                    hay_cambios = True
+    #Se ordena la matriz 
+    ordenar_matriz(matriz, filas, columnas)
+ 
+    #Se muestra el contenido de la mátriz después de su ordenación
+    print("\nDespués de ordenar:\n")
+    dibujar_matriz(matriz)
 
 
-#Se muestra el contenido de la mátriz después de su ordenación
-print(" ")
-print("Después de ordenar: ")
-print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matriz]))
+
+#Función para dibujar la matriz bidimensional en filas y columnas
+def dibujar_matriz(matriz):
+    print('\n'.join(['\t'.join([str(columna) for columna in fila]) for fila in matriz]))
+
+
+
+#Función para ordenar la matriz bidimensional por el metodo de la burbuja
+#La lista de dos dimensiones, al ser un objeto mutable, se está pasando por referencia 
+# y por lo tanto se está modificando aqui directamente, con lo que no es necesario retornarlo
+def ordenar_matriz(matriz, filas, columnas):
+    #Se comparan todos con el siguiente y sólo se para cuando no se hace ningún intercambio
+    hay_cambios = True
+    while(hay_cambios):
+        #Para cada recorrido, inicialmente vamos a definir que no se ha hecho ningún cambio
+        hay_cambios = False
+        
+        #Se recorre cada fila (primera dimensión de la matriz)
+        for i in range(filas):
+            #Se recorre cada columna de cada fila (segunda dimensión de la matriz)
+            for j in range(columnas):
+                #Se ignora el intentar comparar el último elemento con un inexistente siguiente
+                if(not(i == filas - 1 and j == columnas - 1)):
+                    #Definimos índices para el segundo elemento a comparar
+                    k = i
+                    l = j + 1
+
+                    #Si sobrepasa la última columna, cogemos el primer elemento de la siguiente fila 
+                    if(l == columnas):
+                        k += 1
+                        l = 0
+
+                    #Comparamos e intercambiamos si es mayor (y activamos flag)
+                    if(matriz[i][j] >  matriz[k][l]):
+                        auxiliar = matriz[i][j]
+                        matriz[i][j] = matriz[k][l]
+                        matriz[k][l] = auxiliar
+
+                        hay_cambios = True
+
+
+
+
+
+
+#Llamar a función main
+if __name__ == "__main__":
+    main()
