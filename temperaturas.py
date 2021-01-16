0ebe5289a8efb1b1ea68fe38b52d7d7d146e5381
jsanrub275 <a20sanrubj0433@ies-mardecadiz.com>
2021-01-16 01:22:13 +0100

    Esto va teniendo buena pinta

--- temperaturas.py
+++ temperaturas.py
@@ -1,3 +1,16 @@
+"""
+
+Programa para obtener de la API de la AEMET, aprovechando para ello un modulo que existe
+en GitHub, los datos de las temperaturas mínimas y máximas, junto con su correspondiente 
+hora para un rango de fechas dado, para las estaciones de las provincias que se indiquen.
+
+Previamente a su ejecución hay que instalar con pip este mencionado módulo.
+
+https://opendata.aemet.es/centrodedescargas/inicio
+https://github.com/pablo-moreno/python-aemet
+
+"""
+
 #Importar sólo las clases del modulo de AEMET que se van a utilizar
 from aemet import Aemet, Estacion
 import json
@@ -35,20 +48,12 @@ def main():
     for estacion in estaciones:
         #Sólo se toman las que nos interesan
         if estacion["provincia"] in ANDALUCIA:
-            #print(estacion)
             #print("Provincia: {} Indicativo: {} Nombre: {}".format(estacion["provincia"], \estacion["indicativo"], estacion["nombre"]))
             valores_climatologicos_diarios = aemet.get_valores_climatologicos_diarios(fecha_inicio, fecha_fin, estacion["indicativo"])
-            #print(valores_climatologicos_diarios)
-            # {'descripcion': 'No hay datos que satisfagan esos criterios', 'estado': 404}
 
-            #No hay datos para para esa estación en el rango de fechas que se ha pasado
+            #No hay datos para esa estación en el rango de fechas que se ha pasado
             if isinstance(valores_climatologicos_diarios, dict) and "estado" in valores_climatologicos_diarios:
-                """
-                print("Descripción: {} - Estado: {}".format(\
-                    valores_climatologicos_diarios["descripcion"], \
-                    valores_climatologicos_diarios["estado"]))
-                """
-                pass
+                mostrar_no_hay_datos_estacion_en_rango_fechas(valores_climatologicos_diarios, estacion, FECHA_INICIO, FECHA_FIN)
             else:
                 #Recorrer lista para coger de cada entrada los valores que interesan
                 for valor_climatologico_diario in valores_climatologicos_diarios:
@@ -57,15 +62,12 @@ def main():
                     datos_temperaturas.append(obtener_registro_temperaturas(estacion, valor_climatologico_diario, datos_temperaturas))
 
     #Una vez se tienen todos los datos de las estaciones, se muestran o se salvan
-    if not datos_temperaturas:
-        print("No hay datos para NINGUNA estación entre las fechas {} y {}.".format(FECHA_INICIO, FECHA_FIN))
-    else:
-        #print(json.dumps(datos_temperaturas, ensure_ascii=False, indent=2)).encode("utf8")
+    procesar_datos_temperaturas(datos_temperaturas)
 
-        with open("datos_temperaturas", "w", encoding="utf-8") as fichero:
-            json.dump(datos_temperaturas, fichero, ensure_ascii=False, indent=4)
-
-
+    if datos_temperaturas:
+        datos_minimas_maximas_temperaturas = obtener_minima_maxima_por_dia_y_provincia(datos_temperaturas[:])
+        for dato_minimas_maximas_temperuras in datos_minimas_maximas_temperaturas:
+            print(dato_minimas_maximas_temperuras)
 
 
 ### -------------------------------------------------- obtener_registro_temperaturas() -------------------------------------------------- ###
@@ -110,8 +112,80 @@ def mostrar_datos_estacion_dia(estacion, valor_climatologico_diario):
 
 
 
+### -------------------------------------------------- procesar_datos_temperaturas() -------------------------------------------------- ###
+#Mostrar y/o grabar en fichero los datos de temperaturas que se han obtenido
+def procesar_datos_temperaturas(datos_temperaturas):
+    if not datos_temperaturas:
+        print("No hay datos para NINGUNA estación entre las fechas {} y {}.".format(FECHA_INICIO, FECHA_FIN))
+    else:
+        #print(json.dumps(datos_temperaturas, ensure_ascii=False, indent=2)).encode("utf8")
+
+        with open("datos_temperaturas", "w", encoding="utf-8") as fichero:
+            json.dump(datos_temperaturas, fichero, ensure_ascii=False, indent=4)
 
 
+
+### -------------------------------------------------- mostrar_no_hay_datos_estacion_en_rango_fechas() -------------------------------------------------- ###
+#Mostrar que no hay datos para esa estación en el rango de fechas que se ha pasado
+def mostrar_no_hay_datos_estacion_en_rango_fechas(valores_climatologicos_diarios, estacion, fecha_inicio, fecha_fin):
+    print("No hay datos de temperaturas entre el {} y el {}, para la estación {} de la pronvincia de {} y ubicada en {}.".format(\
+        fecha_inicio, \
+        fecha_fin, \
+        estacion["indicativo"], \
+        estacion["provincia"], \
+        estacion["nombre"],))
+
+
+
+
+def obtener_minima_maxima_por_dia_y_provincia(datos_temperaturas):
+    datos_minimas_maximas_temperaturas = []
+
+    for dato_temperaturas in datos_temperaturas:
+        provincia = dato_temperaturas["provincia"]
+        fecha     = dato_temperaturas["fecha"]
+        tmin      = dato_temperaturas["minima"]
+        horatmin  = dato_temperaturas["hora_minima"]
+        tmax      = dato_temperaturas["maxima"]
+        horatmax  = dato_temperaturas["hora_maxima"]
+
+        indice = next((index for (index, dato) in enumerate(datos_minimas_maximas_temperaturas) \
+                       if dato["provincia"] == provincia and dato["fecha"] == fecha), None)
+
+        if not indice:
+            registro_temperaturas = {
+                    "provincia":   provincia,
+                    "fecha":       fecha,
+                    "minima":      tmin,
+                    "hora_minima": horatmin,
+                    "maxima":      tmax,
+                    "hora_maxima": horatmax
+                }
+
+            datos_minimas_maximas_temperaturas.append(registro_temperaturas)
+        else:
+            #print(indice, provincia, fecha, tmin, datos_minimas_maximas_temperaturas[indice]["minima"], tmax, datos_minimas_maximas_temperaturas[indice]["maxima"] )
+            if tmin and tmin < datos_minimas_maximas_temperaturas[indice]["minima"]:
+                datos_minimas_maximas_temperaturas[indice]["minima"]      = tmin
+                datos_minimas_maximas_temperaturas[indice]["hora_minima"] = horatmin
+
+            if tmax and tmax > datos_minimas_maximas_temperaturas[indice]["maxima"]:
+                datos_minimas_maximas_temperaturas[indice]["maxima"]      = tmax
+                datos_minimas_maximas_temperaturas[indice]["hora_maxima"] = horatmax
+
+    return sorted(datos_minimas_maximas_temperaturas, key=lambda k: (k["provincia"], k["fecha"]))
+
+
+
+
+
+##########################################################################################
+#                                                                                        #
+#                                                                                        #
+#                                      M  A  I  N                                        #
+#                                                                                        #
+#                                                                                        #
+##########################################################################################
 #Llamar a función main
 if __name__ == "__main__":
     main()  
